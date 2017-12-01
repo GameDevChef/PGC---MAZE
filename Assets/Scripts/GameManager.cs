0353e8e782deb563ab7112abba636d7e1076736e
DESKTOP-Darek <darek710@wp.pl>
2017-12-01 14:08:29 +0100

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Maze MazePrefab;
+
+    private Maze m_mazeInstance;
+
+    private void Start()
+    {
+        BeginGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            RestartGame();
+    }
+
+    private void RestartGame()
+    {
+        StopAllCoroutines();
+        Destroy(m_mazeInstance.gameObject);
+        BeginGame();
+    }
+
+    private void BeginGame()
+    {
+        m_mazeInstance = Instantiate(MazePrefab) as Maze;
+        StartCoroutine(m_mazeInstance.Generate());
+    }
+}
